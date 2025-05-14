a9d6f14535182023c83084ce39134da28a9fabc4
Helarman <helarman@bk.ru>
2025-05-15 02:27:41 +0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as crypto from 'crypto';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://crm-frontend-v7zf.onrender.com'],
     credentials: true,
     allowedHeaders: [
       'Authorization',
