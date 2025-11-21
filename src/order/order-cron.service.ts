ad5e924842c41cde26436b99ae4dd30b29a5ba86
Helarman <helarman@bk.ru>
2025-11-21 10:05:39 +0300

    fix: preorder cron orderitem

--- src/order/order-cron.service.ts
+++ src/order/order-cron.service.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { PrismaService } from '../prisma.service';
 import { OrderGateway } from './order.gateway';
 import { OrderService } from './order.service';
+import { EnumOrderItemStatus } from '@prisma/client';
 
 @Injectable()
 export class OrderSchedulerService implements OnModuleInit {
@@ -18,73 +19,108 @@ export class OrderSchedulerService implements OnModuleInit {
     this.logger.log('Order Scheduler Service initialized');
   }
 
-@Cron(CronExpression.EVERY_MINUTE) 
-async handleScheduledOrders() {
-  const now = new Date();
-  this.logger.debug(`Order auto scheduler. Time: ${now}`);
-
-  try {
+  @Cron(CronExpression.EVERY_MINUTE) 
+  async handleScheduledOrders() {
     const now = new Date();
-    const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
+    this.logger.debug(`Order auto scheduler. Time: ${now}`);
 
-    const ordersToUpdate = await this.prisma.order.findMany({
-      where: {
-        status: 'CONFIRMED',
-        scheduledAt: {
-          not: null,
-          gte: oneHourLater
-        },    
-      },
-      include: {
-        items: true,
-        restaurant: {
-          include: {
-            network: {
-              include: {
-                tenant: true
+    try {
+      const now = new Date();
+      const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
+
+      const ordersToUpdate = await this.prisma.order.findMany({
+        where: {
+          status: 'CONFIRMED',
+          scheduledAt: {
+            not: null,
+            gte: oneHourLater
+          },    
+        },
+        include: {
+          items: {
+            where: {
+              status: {
+                in: ['CREATED'] 
+              }
+            }
+          },
+          restaurant: {
+            include: {
+              network: {
+                include: {
+                  tenant: true
+                }
               }
             }
           }
+        },
+      });
+
+      this.logger.log(`Found ${ordersToUpdate.length} orders to update to PREPARING`);
+
+      for (const order of ordersToUpdate) {
+        try {
+          this.logger.log(`Updating order ${order.number} to PREPARING (scheduled at: ${order.scheduledAt}, current time: ${now})`);
+          
+          const updatedOrder = await this.prisma.$transaction(async (prisma) => {
+            const updatedOrder = await prisma.order.update({
+              where: { id: order.id },
+              data: { status: 'PREPARING' },
+              include: this.orderService['getOrderInclude'](), 
+            });
+
+            if (order.items.length > 0) {
+              await prisma.orderItem.updateMany({
+                where: { 
+                  orderId: order.id,
+                  status: {
+                    in: ['CREATED',] 
+                  }
+                },
+                data: { 
+                  status: EnumOrderItemStatus.IN_PROGRESS,
+                  startedAt: new Date()
+                },
+              });
+            }
+
+            return updatedOrder;
+          });
+
+          const response = this.orderService['mapToResponse'](updatedOrder);
+          
+          await this.orderGateway.notifyOrderStatusUpdated(response);
+          
+          for (const item of order.items) {
+            await this.orderGateway.notifyOrderItemStatusUpdated(response, item.id);
+          }
+          
+          this.logger.log(`Successfully updated order ${order.number} to PREPARING and ${order.items.length} items to IN_PROGRESS`);
+        } catch (error) {
+          this.logger.error(`Failed to update order ${order.number}: ${error.message}`);
         }
-      },
-    });
-
-    this.logger.log(`Found ${ordersToUpdate.length} orders to update to PREPARING`);
-
-    for (const order of ordersToUpdate) {
-      try {
-        this.logger.log(`Updating order ${order.number} to PREPARING (scheduled at: ${order.scheduledAt}, current time: ${now})`);
-        
-        const updatedOrder = await this.prisma.order.update({
-          where: { id: order.id },
-          data: { status: 'PREPARING' },
-          include: this.orderService['getOrderInclude'](), 
-        });
-
-        const response = this.orderService['mapToResponse'](updatedOrder);
-        
-        await this.orderGateway.notifyOrderStatusUpdated(response);
-        
-        this.logger.log(`Successfully updated order ${order.number} to PREPARING`);
-      } catch (error) {
-        this.logger.error(`Failed to update order ${order.number}: ${error.message}`);
       }
-    }
 
-    if (ordersToUpdate.length > 0) {
-      this.logger.log(`Processed ${ordersToUpdate.length} scheduled orders`);
+      if (ordersToUpdate.length > 0) {
+        this.logger.log(`Processed ${ordersToUpdate.length} scheduled orders`);
+      }
+    } catch (error) {
+      this.logger.error(`Error in scheduled orders handler: ${error.message}`);
     }
-  } catch (error) {
-    this.logger.error(`Error in scheduled orders handler: ${error.message}`);
   }
-}
 
   async moveToPreparingImmediately(orderId: string): Promise<void> {
     try {
       const order = await this.prisma.order.findUnique({
         where: { id: orderId },
         include: {
-          items: true,
+          items: {
+            where: {
+              status: {
+                in: ['CREATED']
+              }
+            }
+          },
           restaurant: {
             include: {
               network: {
@@ -109,16 +145,40 @@ async handleScheduledOrders() {
         throw new Error('Order does not have scheduled time');
       }
 
-      const updatedOrder = await this.prisma.order.update({
-        where: { id: orderId },
-        data: { status: 'PREPARING' },
-        include: this.orderService['getOrderInclude'](),
+      const updatedOrder = await this.prisma.$transaction(async (prisma) => {
+        const updatedOrder = await prisma.order.update({
+          where: { id: orderId },
+          data: { status: 'PREPARING' },
+          include: this.orderService['getOrderInclude'](),
+        });
+
+        if (order.items.length > 0) {
+          await prisma.orderItem.updateMany({
+            where: { 
+              orderId: orderId,
+              status: {
+                in: ['CREATED']
+              }
+            },
+            data: { 
+              status: EnumOrderItemStatus.IN_PROGRESS,
+              startedAt: new Date()
+            },
+          });
+        }
+
+        return updatedOrder;
       });
 
       const response = this.orderService['mapToResponse'](updatedOrder);
+      
       await this.orderGateway.notifyOrderStatusUpdated(response);
+      
+      for (const item of order.items) {
+        await this.orderGateway.notifyOrderItemStatusUpdated(response, item.id);
+      }
 
-      this.logger.log(`Order ${order.number} moved to PREPARING immediately`);
+      this.logger.log(`Order ${order.number} moved to PREPARING immediately and ${order.items.length} items to IN_PROGRESS`);
     } catch (error) {
       this.logger.error(`Failed to move order to PREPARING: ${error.message}`);
       throw error;
